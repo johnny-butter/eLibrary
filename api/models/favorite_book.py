59670ac26bbd432c5b3f632d15a45792df727299
johnny-butter <butter3126@gmail.com>
2019-11-10 13:14:38 +0800

    [model] add scope

--- api/models/favorite_book.py
+++ api/models/favorite_book.py
@@ -1,10 +1,32 @@
 from django.db import models
 
 
+class favoriteQuerySet(models.QuerySet):
+    def favorited(self, user):
+        return self.filter(isFavorite=True).filter(username=user)
+
+    def not_favorited(self):
+        return self.filter(isFavorite=False)
+
+
+class favoriteManager(models.Manager):
+    def get_queryset(self):
+        return favoriteQuerySet(self.model, using=self._db)
+
+    def favorited(self):
+        return self.get_queryset().favorited()
+
+    def not_favorited(self):
+        return self.get_queryset().not_favorited()
+
+
 class favoriteBook(models.Model):
     bookname = models.ForeignKey('Book', models.PROTECT)
     username = models.ForeignKey('User', models.PROTECT)
     isFavorite = models.BooleanField()
+
+    # objects = favoriteManager()
+    objects = favoriteQuerySet.as_manager()
 
     class Meta:
         db_table = 'favorite_book'
