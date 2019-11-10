7bc126570c8a2a9f29301f9eecede22fc9a2b2fb
johnny-butter <butter3126@gmail.com>
2019-11-10 13:14:08 +0800

    [doc] modify settings location

--- eLibrary/celery.py
+++ eLibrary/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eLibrary.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 app = Celery('eLibrary')
 app.config_from_object('django.conf:settings', namespace='CELERY')
