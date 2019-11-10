e6bcee214f06189793199902c1f1f88b524fb0c7
johnny-butter <butter3126@gmail.com>
2019-11-10 13:15:28 +0800

    [view] remove redundant lib

--- loginPage/views.py
+++ loginPage/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from jsonreader import JsonReader
 from django.urls import reverse
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect, HttpResponseBadRequest
 import requests
