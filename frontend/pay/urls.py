ddd0ff4a5bbf765826ce20376d9cd955050c02f9
johnny-butter <butter3126@gmail.com>
2020-08-19 07:59:12 +0800

    [refactor] modify payPage to pay_page

--- frontend/pay/urls.py
+++ frontend/pay/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('purchase/', views.payPage, name='purchasePage')
+    path('purchase/', views.pay_page, name='pay_page')
 ]
